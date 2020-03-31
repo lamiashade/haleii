d47c247adefec53713f29d0435cbfc383b6efb33
lamiashade <cestfrankct@gmail.com>
2020-03-31 00:01:30 -0400

    image size updates

--- src/components/cards/CardUI.js
+++ src/components/cards/CardUI.js
@@ -4,7 +4,7 @@ const Card = props => {
     return(
         <React.Fragment>  
             <div className="card">
-                <img src={props.imgsrc} alt="" lassName="overflow" />
+                <img src={props.imgsrc} alt="" className="overflow" />
             </div>
             <h4 className="card-title">{props.title}</h4>
         </React.Fragment>
